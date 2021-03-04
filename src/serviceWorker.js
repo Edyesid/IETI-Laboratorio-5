bcbf1f4aab93793d5b44be9796afe703278cf0ce
Edyesid <54111499+Edyesid@users.noreply.github.com>
2021-03-04 00:14:08 -0500

    parte II

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -18,16 +18,20 @@ const isLocalhost = Boolean(
     )
 );
 
-export default function register() {
+export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
         // The URL constructor is available in all browsers that support SW.
-        const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
+        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
         if (publicUrl.origin !== window.location.origin) {
             // Our service worker won't work if PUBLIC_URL is on a different origin
             // from what our page is served on. This might happen if a CDN is used to
-            // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+            // serve assets; see https://github.com/facebook/create-react-app/issues/2374
             return;
         }
+        //Add this part of code to your serviceWorker.js
+        window.addEventListener('install', () => {
+            console.log("install!!");
+        });
 
         window.addEventListener('load', () => {
             const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
